94eab9823364ae14976a97b3235efea1ca4b7c44
chaojie-fu <fuchaojiemail@gmail.com>
2019-08-09 11:39:58 +0800

    first executable control file

--- control.py
+++ control.py
@@ -1,18 +1,74 @@
 import pybullet as p
 import time
 import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+import numpy as np
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -10)
+p.setPhysicsEngineParameter(numSolverIterations=50)
+p.setTimeOut(40000000)
+p.setTimeStep(1./100)
+
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
-for i in range (100):
+robotStartPos = [0, 0, 0.5]
+robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
+robotId = p.loadURDF("robot.urdf", robotStartPos, robotStartOrientation)
+
+nJoints = p.getNumJoints(robotId)
+jointNameToId = {}
+for j in range(nJoints):
+    jointInfo = p.getJointInfo(robotId, j)
+    jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
+
+bodyLF = jointNameToId['bodyLF']
+bodyRF = jointNameToId['bodyRF']
+bodyLB = jointNameToId['bodyLB']
+bodyRB = jointNameToId['bodyRB']
+hipLF = jointNameToId['hipLF']
+hipRF = jointNameToId['hipRF']
+hipLB = jointNameToId['hipLB']
+hipRB = jointNameToId['hipRB']
+kneeLF = jointNameToId['kneeLF']
+kneeRF = jointNameToId['kneeRF']
+kneeLB = jointNameToId['kneeLB']
+kneeRB = jointNameToId['kneeRB']
+
+legnumbering = [
+    bodyLF, bodyRF, bodyLB, bodyRB,
+    hipLF, hipRF, hipLB, hipRB,
+    kneeLF, kneeRF, kneeLB, kneeRB
+]
+
+halfpi = 1.57079632679
+twopi = 4 * halfpi
+pi = 2 * halfpi
+motordir = [halfpi, -halfpi, -halfpi, halfpi,
+            halfpi, -halfpi, halfpi, -halfpi,
+            -halfpi, halfpi, -halfpi, halfpi
+]
+
+para = [
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+]
+
+control = np.multiply(motordir,para)
+
+#initialize
+for i in range(12):
+    p.setJointMotorControl2(
+        bodyIndex=robotId,
+        jointIndex=legnumbering[i],
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=control[i],
+        positionGain=1,
+        velocityGain=0.5,
+        force=10
+    )
+
+for i in range (10000):
+
     p.stepSimulation()
-    time.sleep(1./240.)
-info = p.getJointInfo(boxId,0)
-info2 = p.getJointInfo(boxId,14)
+    time.sleep(1./100.)
 p.disconnect()
-print(info)
-print(info2)
